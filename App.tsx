49b159e149c330251d8c73244e89b2b85335942d
Jonas Boone <boone.jonas@outlook.be>
2023-06-04 15:41:36 +0200

    feat(homescreen): add basic UI

--- App.tsx
+++ App.tsx
@@ -1,21 +1,67 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={containerStyles.wrapper}>
+      <View style={headingStyles.heading}>
+        <Text style={headingStyles.temperature}>6</Text>
+        <Text style={headingStyles.feels}>Feels like 5</Text>
+        <View style={headingStyles.highAndLowWrapper}>
+          <Text style={headingStyles.highAndLow}>High: 8</Text>
+          <Text style={headingStyles.highAndLow}>Low: 4</Text>
+        </View>
+      </View>
+      <View style={bodyStyles.body}>
+        <Text style={bodyStyles.description}>It&apos;s sunny</Text>
+        <Text style={bodyStyles.message}>
+          It&apos;s perfect t-shirt weather
+        </Text>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
+const containerStyles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: "pink",
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
+
+const headingStyles = StyleSheet.create({
+  heading: {
+    alignItems: "center",
+    flex: 1,
+  },
+  temperature: {
+    color: "black",
+    fontSize: 48,
+  },
+  feels: {
+    fontSize: 30,
+    color: "black",
+  },
+  highAndLow: {
+    color: "black",
+    fontSize: 20,
+  },
+  highAndLowWrapper: {
+    flexDirection: "row",
+    columnGap: 20,
+  },
+});
+
+const bodyStyles = StyleSheet.create({
+  body: {
+    // flex: 4,
+  },
+  description: {
+    fontSize: 48,
+  },
+  message: {
+    fontSize: 32,
+  },
+});
+
+export default App;
